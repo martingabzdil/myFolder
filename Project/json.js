4cf451716ee2599d1aeda7335f6207001de052d7
romanV <soundofmemories@gmail.com>
2014-11-14 16:23:09 +0100

    piatok
    priadana aka-taka paginacia a obrazky

--- Project/json.js
+++ Project/json.js
@@ -4,35 +4,58 @@ function loadJSON(){
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function(){
 		if (xmlhttp.readyState==4 && xmlhttp.status==200){
-			var subor = JSON.parse(xmlhttp.responseText);
+			subor = JSON.parse(xmlhttp.responseText);
 			fillInfo(subor);
+			createNavBtns(subor);
 		}
 	}
 	xmlhttp.open("GET","http://academy.tutoky.com/api/json.php",true);
 	xmlhttp.send();
 }
 
-function fillInfo(subor){
-
+function fillInfo(subor,page){
+	page=page||0;
 	var content="";
-	for(var i=0;i<10;i++){
-		content+='<article class="video">';
-		content+='<div class="overlay">';
-		content+='<h2>Check out this cool video!</h2>';
-		content+='</div><div class="video-still-package">';
-		content+='<img class="still" src="a7a.png"alt=""/>';
-		content+='<div class="play-button">';
-		content+='</div></div><section class="textblock">';
-		content+='<h2 class="titulok truncate" id="nadpis">';
-		content+=subor[i].title;
-		content+='</h2><time class="date" id="timestamp">';
-		content+=convertTime(subor[i].timestamp);
-		content+='</time></section></article>';
-	}
-
+	var noOfItems = 0*1;
 	
+	if (page*10+10>subor.length){
+		noOfItems=subor.length;
+	} else {
+		noOfItems=(page*10)+10;
+	}
+		for(var i=(page*10);i<noOfItems;i++){
+			content+='<article class="video">';
+			content+='<div class="overlay">';
+			content+='<h2>Check out this cool video!</h2>';
+			content+='</div><div class="video-still-package">';
+			content+='<img class="still" src="_img/'
+			content+=subor[i].image;
+			content+='.jpg"alt=""/>';
+			content+='<div class="play-button">';
+			content+='</div></div><section class="textblock">';
+			content+='<h2 class="titulok truncate" id="nadpis">';
+			content+=subor[i].title;
+			content+='</h2><time class="date" id="timestamp">';
+			content+=convertTime(subor[i].timestamp);
+			content+='</time></section></article>';
+	}
+	
+
+
 	document.getElementById("main-content").innerHTML=content;
-	console.log(content);
+	
+}
+
+function createNavBtns(subor){
+	var buttonBlock="";
+	for (var i =0;i<(subor.length)/10;i++){
+		buttonBlock+='<div class="navitems" id="btn'+i+'" onclick="loadItems('+i+');">'+(i+1)+'</div>';
+	}
+	document.getElementById("pgbtn").innerHTML=buttonBlock; 
+}
+
+function loadItems(btnId){
+	fillInfo(subor,btnId);
 }
 
 var convertTime = function(JSONtimestamp){
@@ -86,4 +109,3 @@ var convertTime = function(JSONtimestamp){
 	// formattedDate = formattedDate + " " + formattedTime;
 	return formattedDate;
 }
-
