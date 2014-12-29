0a3995b86b396ea8717c6097d3ebf1a318f133b2
romanV <soundofmemories@gmail.com>
2014-12-29 11:55:15 +0100

    filter

--- AngularTutoky/app.js
+++ AngularTutoky/app.js
@@ -29,53 +29,54 @@
     function($window, $scope) {
       $scope.CurrentPage = 0;
       $scope.itemsPerPage = 10;
-      $scope.selected = 0;
+      $scope.selected =0;
 
       $scope.wHeight = $window.innerWidth;
-            //select
-        $scope.categories = [{
+
+      $scope.categories = [{
             "value": 0,
             "text": "All"
-        }, {
-            "value": 1,
-            "text": "Football"
-        }, {
-            "value": 2,
-            "text": "Handball"
-        }, {
-            "value": 3,
-            "text": "News"
-        }, {
-            "value": 4,
-            "text": "Hockey"
-        }, {
-            "value": 5,
-            "text": "Basketball"
-        }, {
-            "value": 6,
-            "text": "Volleyball"
-        }, {
-            "value": 7,
-            "text": "Galleries"
-        }, ];
+        }]
 
-            // $scope.filterByCategory = function(selectedParam) {
-            //     // console.log(selectedParam);
-            //     $scope.selected = selectedParam;
-            //     console.log($scope.selected);
-            // };
-          }
-          ]);
+    }]);
 
 app.controller('displayArticles', ['$scope', 'LoadJson',
   function($scope, LoadJson, CurrentPage) {
 
     LoadJson.success(function(data) {
       $scope.data = data;
-      $scope.dataToBeDisplayed=[];
-      // $scope.subData = [];
-      $scope.subData=$scope.data;
+      $scope.dataToBeDisplayed = [];
+      $scope.subData = $scope.data;
+
+      $scope.categories.push(
+         {
+            "value": 1,
+            "text": "Football"
+        })
       
+      var categoriesinJson=[];
+      //get all categories from JSON feed into an array
+     
+      // for (var p in data){
+      //   console.log('objekt c '+p)
+      //   if (data[p].categories.length > 3){
+      //     // console.log(data[p].categories);
+      //     for (var j in data[p].categories){
+      //       if (categoriesinJson.length < 1){
+      //         categoriesinJson.push(data[p].categories[j]);
+      //       } else {
+      //         for (var k in categoriesinJson){
+      //           console.log(k);
+               
+      //         }
+      //       }
+      //       // console.log(j);
+      //     }
+      //   }
+      // }
+
+      console.log(categoriesinJson);
+
 
       $scope.$watch('CurrentPage', function() {
         
@@ -101,47 +102,55 @@ app.controller('displayArticles', ['$scope', 'LoadJson',
  
         }
         
-    });
+      });
 
-    $scope.$watch('selected', function() {
-      console.log('Selected has been changed to '+$scope.selected);
-      $scope.dataToBeDisplayed = [];
-
-      $scope.subData = [];
-      if($scope.selected === 0){
-        $scope.subData=$scope.data;
-          
-      }
-
-      if($scope.selected === 1){
-          
-        for(var k in $scope.data){
-            
-          if($scope.data[k].image==='1'){
-
-            
-            $scope.subData.push($scope.data[k]);
-            
-          }
-            
-        }
-
-   
-      }
-
-      
-      for (var j = 0; j < 10; j++) { //repair number of rendered articles
-
-          $scope.dataToBeDisplayed.push($scope.subData[j]);
- 
-        }
+      $scope.$watch('selected', function() {
         
+        $scope.dataToBeDisplayed = [];
+
+        $scope.subData = [];
+      
+        if($scope.selected === 0){
+          
+          $scope.subData=$scope.data;
+          
+        }
+
+        if($scope.selected === 1){
+            
+          for(var k in $scope.data){
+              
+            if($scope.data[k].image==='1'){
+
+              
+              $scope.subData.push($scope.data[k]);
+              
+            }
+              
+          }
+     
+        }
+
+        if ($scope.subData.length < $scope.itemsPerPage){
+             for (var j = 0; j < $scope.subData.length; j++) {
+
+            $scope.dataToBeDisplayed.push($scope.subData[j]);
+          }
+        } else {
+            for (var j = 0; j < 10; j++) { 
+
+            $scope.dataToBeDisplayed.push($scope.subData[j]);
+   
+          }
+        }
+
+        $scope.CurrentPage=0;
+
       })
 
       $scope.filterByCategory = function(selectedParam) {
         $scope.selected = selectedParam;  
       };
-
 
       $scope.ConvertTime = function(JSONtimestamp) {
         var d = new Date(parseInt(JSONtimestamp, 10));
@@ -165,6 +174,7 @@ app.controller('Pagination', ['$window', '$scope', 'LoadJson',
   function($window, $scope, LoadJson, CurrentPage) {
     var previousPage = 0;
 
+
     $scope.renderBtns = function(btn) {
 
 
@@ -180,7 +190,7 @@ app.controller('Pagination', ['$window', '$scope', 'LoadJson',
         return btn + 1;
       }
 
-      if (btn === Math.floor($scope.subData.length / $scope.itemsPerPage) - oddVsEvenDecider) {
+      if ($scope.subData.length > $scope.itemsPerPage && btn === Math.floor($scope.subData.length / $scope.itemsPerPage) - oddVsEvenDecider) {
         return btn + 1;
       }
 
@@ -221,19 +231,22 @@ app.controller('Pagination', ['$window', '$scope', 'LoadJson',
     LoadJson.success(function(data) {
       $scope.data = data;
       $scope.noOfPages = [];
-
+      
+      function reloadNoOfPages(){
+        $scope.noOfPages = [];
+        for (var i = 0; i < ($scope.subData.length / $scope.itemsPerPage); i++) {
+            $scope.noOfPages.push(i);
+        }
+      }
 
       $scope.$watch('CurrentPage', function() {
-
-        if ($scope.CurrentPage === 0) {
-
-          $scope.noOfPages = [];
-          for (var i = 0; i < ($scope.subData.length / $scope.itemsPerPage); i++) {
-            $scope.noOfPages.push(i);
-          }
-        }
-
+          reloadNoOfPages();
       });
+
+      $scope.$watch('selected', function() {
+        reloadNoOfPages();
+      })
+
 
       $scope.nextPage = function() {
         var oddVsEvenDecider = 0;
